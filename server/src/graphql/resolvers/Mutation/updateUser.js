e6bc1776f609e41bd1628b1ce37c59bdc0e1a780
Ndako Muhammad Z <mzndako@gmail.com>
2019-03-18 21:25:58 +0100

    Throws friendly error when updating non-existant user id and updates data not overwrite

--- server/src/graphql/resolvers/Mutation/updateUser.js
+++ server/src/graphql/resolvers/Mutation/updateUser.js
@@ -1,13 +1,23 @@
 import { setUser } from '../../../helpers';
+import getUser from '../../../helpers/getUser';
 
 export default async function User(root, { user }, { ctx }, info) {
-  // todo: 1 this throws a unfriendly (and potentially unsafe) error if a non-existnant user ID is entered.
+  // DONE: 1 this throws a unfriendly (and potentially unsafe) error if a non-existnant user ID is entered.
   // how can we check for a non-existing user id and throw a more friendly error.
-
-  // todo: 2 why is this update overwriting existing user data? Need to fix this so that just data input is
+  
+  let userDetails = await getUser(user.id);
+  if(user.name){
+    userDetails.name = user.name;
+  }
+  if(user.email){
+    userDetails.email = user.email;
+  }
+  // DONE: 2 why is this update overwriting existing user data? Need to fix this so that just data input is
   // updated rather than overwritting all the data.
 
-  await setUser(user);
-
+  await setUser(userDetails);
+  // }catch(error){
+    // return false;
+  // }
   return true;
 }
--- server/src/helpers/getUser.js
+++ server/src/helpers/getUser.js
@@ -1,8 +1,14 @@
 import fs from 'fs';
 import util from 'util';
 const readFile = util.promisify(fs.readFile);
+const exists = util.promisify(fs.exists);
 
 export default async function getUser(id) {
+  let fileExists = await exists(`./data/users/${id}.json`);
+
+  if(!fileExists){
+    throw 'User does not exists';
+  }
   const data = await readFile(`./data/users/${id}.json`, 'utf8');
   return JSON.parse(data);
 }

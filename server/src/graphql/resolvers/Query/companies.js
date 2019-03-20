a47ba53d88aff881ce9303ffc5c59b1a4a41dc96
Ndako Muhammad Z <mzndako@gmail.com>
2019-03-20 02:54:05 +0100

    Optimized Company query

--- server/src/graphql/resolvers/Query/companies.js
+++ server/src/graphql/resolvers/Query/companies.js
@@ -3,23 +3,18 @@ import util from 'util';
 
 const readDir = util.promisify(fs.readdir);
 
-import { getCompany, getUser } from '../../../helpers';
+import { getCompany, getUser, getAllUsers } from '../../../helpers';
 
 export default async function companies(root, args, { ctx }, info) {
-  // Read all the users
-  let files = await readDir('./data/users');
-  let users = files
-    .filter(filename => filename.includes('.json'))
-    .map(filename => getUser(filename.replace('.json', '')));
 
-  files = await readDir('./data/companies');
-  let companies = files
+  const files = await readDir('./data/companies');
+
+  const companies = files
     .filter(filename => filename.includes('.json'))
     .map(filename => getCompany(filename.replace('.json', '')));
-  
+
   // Attach all users to the context object  
-  ctx.users = await Promise.all(users);
-  
-  
+  ctx.users = await getAllUsers()
+
   return companies;
 }
--- server/src/graphql/resolvers/Query/company.js
+++ server/src/graphql/resolvers/Query/company.js
@@ -1,5 +1,9 @@
-import { getCompany } from '../../../helpers';
+import { getCompany, getAllUsers } from '../../../helpers';
 
 export default async function company(root, { id }, { ctx }, info) {
+  
+  // Attach all users to the context to be used by the employee resolver
+  ctx.users = await getAllUsers();
+
   return getCompany(id);
 }
